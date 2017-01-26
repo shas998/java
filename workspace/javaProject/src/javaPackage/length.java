e43b52a236b202fbe611aaf4b7434ce8d305486c
shas998 <shashikantrai998@yahoo.com>
2017-01-26 22:20:24 +0530

    char length

---
+++ workspace/javaProject/src/javaPackage/length.java
@@ -0,0 +1,16 @@
+package javaPackage;
+
+public class length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char a[] = {'a','b','c','d','e','f'};
+		
+		for(int i = 0; i<a.length;i++){
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
