7bc33675b712b6570a79e2ceb3de57642b4e0ac1
shas998 <shashikantrai998@yahoo.com>
2017-01-26 22:08:57 +0530

    for loop

---
+++ workspace/javaProject/src/javaPackage/loop.java
@@ -0,0 +1,12 @@
+package javaPackage;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++){
+    System.out.println(i);
+	}
+	}
+
+}
