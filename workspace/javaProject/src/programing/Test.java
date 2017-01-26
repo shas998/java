b870ae963f2088f23f745d46063eaa977c77522b
shas998 <shashikantrai998@yahoo.com>
2017-01-26 22:45:05 +0530

    add

---
+++ workspace/javaProject/src/programing/Test.java
@@ -0,0 +1,18 @@
+package programing;
+
+public class Test {
+static int a=3;
+static int b=4;
+static int c;
+	public static void main(String args[]){
+		System.out.println();
+		c =  (a +  b);
+		System.out.println(c);
+		
+	}
+	
+
+		
+		 
+	
+}
